2b5c6b3e89213e60119ea2c63679c1b39bcf77d4
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-02-15 19:52:23 +0000

    Saving email, username & password to database

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const crypto = require('crypto').randomBytes(256).toString('hex');
+
+// Export config object
+module.exports = {
+    uri: 'mongodb://localhost:27017/mean-angular-2', // Databse URI and database name
+    secret: crypto, // Cryto-created secret
+    db: 'mean-angular-2' // Database name
+  }
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,3 +1,4 @@
+// Imports
 const User = require('../models/user');
 
 module.exports = (router) => {
@@ -13,8 +14,19 @@ module.exports = (router) => {
                 if (!req.body.password) {
                     res.json({ success: false, message: 'You must provide a password' });
                 } else {
-                    console.log(req.body);
-                    res.send('Hello World donal mcgahon');
+                    let user = new User({
+                     email: req.body.email.toLowerCase(),
+                     username: req.body.username.toLowerCase(),
+                     password: req.body.password
+                    });
+                    user.save((err) => {
+                        if (err) {
+                            res.json({ success: false, message: 'Could not save user. Error: ', err
+                        });
+                        } else {
+                            res.json({ success: true, message: 'User Saved'});
+                        }
+                    });
                 }
             }
 
