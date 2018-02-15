ed36ed5394c59bd5179f20d1ac700a059fc19b08
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-02-15 22:09:03 +0000

    Added validation errors for passwords

--- models/user.js
+++ models/user.js
@@ -77,11 +77,52 @@ const usernameValidators = [
     }
   ];
 
+// Validate Function to check password length
+let passwordLengthChecker = (password) => {
+    // Check if password exists
+    if (!password) {
+      return false; // Return error
+    } else {
+      // Check password length
+      if (password.length < 8 || password.length > 35) {
+        return false; // Return error if passord length requirement is not met
+      } else {
+        return true; // Return password as valid
+      }
+    }
+  };
+  
+  // Validate Function to check if valid password format
+  let validPassword = (password) => {
+    // Check if password exists
+    if (!password) {
+      return false; // Return error
+    } else {
+      // Regular Expression to test if password is valid format
+      const regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+      return regExp.test(password); // Return regular expression test result (true or false)
+    }
+  };
+  
+  // Array of Password validators
+  const passwordValidators = [
+    // First password validator
+    {
+      validator: passwordLengthChecker,
+      message: 'Password must be at least 8 characters but no more than 35'
+    },
+    // Second password validator
+    {
+      validator: validPassword,
+      message: 'Must have at least one uppercase, lowercase, special character, and number'
+    }
+  ];
+
 
 const userSchema = new Schema({
 email: { type: String, required: true, unique: true, lowercase: true, validate: emailValidators },
 username: { type: String, required: true, unique: true, lowercase: true, validate: usernameValidators },
-password: { type: String, required: true, }
+password: { type: String, required: true, validate: passwordValidators }
 });
 
 // Middleware to encrypt passwords
--- routes/authentication.js
+++ routes/authentication.js
@@ -44,7 +44,7 @@ module.exports = (router) => {
                             }
                             
                         } else {
-                            res.json({ success: true, message: 'User Saved'});
+                            res.json({ success: true, message: 'Account Registered'});
                         }
                     });
                 }
