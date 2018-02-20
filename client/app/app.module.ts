acca72ebbbdab5a81b12732955b52f7137d88661
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-02-20 16:49:37 +0000

    Added to RegisterComponent

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ApplicationRef }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-import { FormsModule }   from '@angular/forms';
+import { ReactiveFormsModule }   from '@angular/forms';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HomeComponent }  from './home.component';
@@ -16,10 +16,22 @@ import { RegisterComponent }  from './register.component';
 import { routing } from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing, HttpModule, FormsModule],
-  declarations: [AppComponent, HomeComponent, 
-    ProfileComponent, CryptoMapComponent, FriendsComponent,
-    WalletComponent, NewWalletComponent,tradingComponent, SettingsComponent, RegisterComponent ],
+  imports:      [
+     BrowserModule,
+     routing,
+     HttpModule,
+     ReactiveFormsModule],
+  declarations: 
+  [ AppComponent,
+    HomeComponent, 
+    ProfileComponent,
+    CryptoMapComponent,
+    FriendsComponent,
+    WalletComponent,
+    NewWalletComponent,
+    tradingComponent,
+    SettingsComponent,
+    RegisterComponent ],
   exports: [  ],
   providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
--- client/app/register.component.ts
+++ client/app/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: 'register.component.html',
   styleUrls: ['register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  form: FormGroup;
+
+  createForm () {
+    this.form = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: '',
+      confirm: ''
+    })
+  }
+
+  constructor(
+    private formBuilder:  FormBuilder
+  ) {
+    this.createForm()
+   }
+
+  ngOnInit() {
+
+  }
 
 }
