043c8e0f8b5b3a8ae8068b691ebdda38640751f5
DonalMcGahon <dmcgahon1234@hotmail.com>
2018-03-12 16:50:18 +0000

    Image added to home page to access cryptonews

--- client/app/cryptonews/cryptonews.component.js
+++ client/app/cryptonews/cryptonews.component.js
@@ -5,17 +5,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var CryptonewsComponent = /** @class */ (function () {
     function CryptonewsComponent() {
     }
+    CryptonewsComponent.prototype.ngAfterViewInit = function () {
+        !function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0], p = 'https';
+            if (!d.getElementById(id)) {
+                js = d.createElement(s);
+                js.id = id;
+                js.src = p + "://platform.twitter.com/widgets.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }
+        }(document, "script", "twitter-wjs");
+    };
     CryptonewsComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'app-cryptonews',
             templateUrl: './cryptonews.component.html'
-        })
+        }),
+        __metadata("design:paramtypes", [])
     ], CryptonewsComponent);
     return CryptonewsComponent;
 }());
