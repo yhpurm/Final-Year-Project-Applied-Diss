795abb6ca2898772c00055ce48aeb549b4f81296
Conor <conor.tighe22@gmail.com>
2018-03-22 16:06:19 +0000

    Can now check the balance of your wallets

--- client/app/WalletComponents/myWallet.component.js
+++ client/app/WalletComponents/myWallet.component.js
@@ -11,22 +11,48 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var profile_service_1 = require("../services/profile.service");
+var blockchain_service_1 = require("../services/blockchain.service");
+var bal_modal_1 = require("../DataModals/bal.modal");
 var WalletComponent = /** @class */ (function () {
-    function WalletComponent(profileService) {
+    function WalletComponent(blockchainService, profileService) {
+        this.blockchainService = blockchainService;
         this.profileService = profileService;
-        this.wallet = [];
+        this.wallets = [];
     }
-    WalletComponent.prototype.onGetMyWallet = function () {
-        // get users wallet
+    WalletComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.profileService.getMyWallets()
+            .subscribe(function (response) {
+            _this.wallets = response;
+            console.log(_this.wallets);
+            console.log("got wallets");
+        }, function (error) { return console.error(error); });
+    };
+    WalletComponent.prototype.setGuid = function (gid) {
+        console.log("guid: " + gid);
+        this.guid = gid;
+        console.log(this.guid);
+    };
+    WalletComponent.prototype.onGetBal = function () {
+        var _this = this;
+        if (this.guid == null) {
+            return "GUID is empty please select an address";
+        }
+        if (this.pass != this.passvalid) {
+            return "GUID is empty please select an address";
+        }
+        var balrequest = new bal_modal_1.BalanceReq(this.guid, this.pass);
+        this.blockchainService.getBalance(balrequest)
+            .subscribe(function (messages) { return _this.balance = messages; }, function (error) { return console.error(error); });
     };
     WalletComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'Wallet',
             templateUrl: 'myWallet.component.html',
-            providers: [profile_service_1.ProfileService]
+            providers: [blockchain_service_1.BlockchainService, profile_service_1.ProfileService]
         }),
-        __metadata("design:paramtypes", [profile_service_1.ProfileService])
+        __metadata("design:paramtypes", [blockchain_service_1.BlockchainService, profile_service_1.ProfileService])
     ], WalletComponent);
     return WalletComponent;
 }());
--- client/app/WalletComponents/myWallet.component.ts
+++ client/app/WalletComponents/myWallet.component.ts
@@ -1,28 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Profile } from "../DataModals/profile.model";
 import { Wallet } from "../DataModals/mywallet.model";
 import { ProfileService } from "../services/profile.service";
+import { BlockchainService } from '../services/blockchain.service';
+import { BalanceReq } from "../DataModals/bal.modal";
 
 @Component({
   moduleId: module.id,
   selector: 'Wallet',
   templateUrl: 'myWallet.component.html',
-  providers: [ProfileService]
+  providers: [BlockchainService,ProfileService]
 })
 
-export class WalletComponent {
+export class WalletComponent implements OnInit {
 
-  wallet: Wallet[] = [];
+  wallets: Wallet[] = [];
   username: string;
-  walletpass: string;
-  address: string;
   guid: string;
+  pass: string;
+  passvalid: string;
+  address: string;
+  balance: number;
 
-  constructor(private profileService: ProfileService) {}
+  constructor(private blockchainService: BlockchainService, private profileService: ProfileService) {}
 
-  
-  onGetMyWallet() {
-    // get users wallet
+  ngOnInit(){
+
+    this.profileService.getMyWallets()
+        .subscribe(
+            response => {
+                this.wallets = response;
+                console.log(this.wallets);
+                console.log("got wallets");
+            },
+            error => console.error(error)
+         );
+  }
+
+  setGuid(gid: string){
+    console.log("guid: " + gid);
+    this.guid = gid;
+    console.log(this.guid);
+  }
+
+  onGetBal(){
+    if(this.guid == null){
+        return "GUID is empty please select an address"
     }
 
- }
+    if(this.pass != this.passvalid){
+        return "GUID is empty please select an address"
+    }
+
+    const balrequest = new BalanceReq(this.guid,this.pass);
+    this.blockchainService.getBalance(balrequest)
+    .subscribe(
+        messages => this.balance = messages,
+        error => console.error(error)
+    );
+  }
+}
