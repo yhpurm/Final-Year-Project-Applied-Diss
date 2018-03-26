f0de26fc2a35a76ab39eb06cad7ba7b60902f28a
Conor <conor.tighe22@gmail.com>
2018-03-26 17:22:00 +0100

    Global Users component refactored and comments added

--- client/app/MlabsComponents/global.component.ts
+++ client/app/MlabsComponents/global.component.ts
@@ -12,32 +12,36 @@ import { Profile } from "../DataModals/profile.model";
 
 export class GlobalComponent implements OnInit {
 
+    // Modals
     profiles: Profile [] = [];
     constructor(private mlabsService: MlabsService,
         private profileService: ProfileService) {}
 
+    // On component initialization
     ngOnInit(){
     
-            this.mlabsService.getGlobalUsers()
+        // MLABS Service get global users on server
+        this.mlabsService.getGlobalUsers()
            .subscribe(
             res => {
-                    console.log(res);
                     this.profiles = res;
-                    console.log(this.profiles);
+                    console.log("results: " + this.profiles);
                },
                error => console.error("error:" + error)
-            );
-        }
+        );
+    }
 
+    // Add an friend event
     onAddFriend(username: string, aboutMe: string, avatar: number, statusCount: number,
     friendCount: number, isOnline: Boolean, address: string,
     email: string, lat: number, long: number){
 
-        console.log(username,aboutMe,avatar,statusCount,friendCount,isOnline,address,email,lat,long)
-        const newStatusPost = new Profile(username,aboutMe,avatar,statusCount,friendCount,isOnline,address,email,lat,long);
-        this.profileService.addFriend(newStatusPost)
+        // Create new friend modal
+        const newFriendPost = new Profile(username,aboutMe,avatar,statusCount,friendCount,isOnline,address,email,lat,long);
+        // Pass new friend modal to service
+        this.profileService.addFriend(newFriendPost)
         .subscribe(
-            () => console.log('POST from status'),
+            () => console.log('POST to friends'),
             error => console.error(error)
         );
     }
