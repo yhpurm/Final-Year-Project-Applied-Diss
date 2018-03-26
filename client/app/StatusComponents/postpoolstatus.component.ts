2e615d5ae3965713ffa2a0e40781ef4c550ddbdb
Conor <conor.tighe22@gmail.com>
2018-03-26 19:26:37 +0100

    pools status component refactored and comments added

--- client/app/StatusComponents/postpoolstatus.component.ts
+++ client/app/StatusComponents/postpoolstatus.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BlockchainService } from '../services/blockchain.service';
 import { StatusService } from '../services/status.service';
 import { PostPools } from "../DataModals/pools.modal";
+
+// Set variable from map location
 declare var google: any;
 var geocoder = new google.maps.Geocoder();
 
@@ -15,27 +17,29 @@ var geocoder = new google.maps.Geocoder();
 
 export class PoolComponent implements OnInit {
 
-  title: string;
-  text: string;
-  AntPool: number;
-  GBMiners: number;
-  SlushPool: number;
-  KanoPool: number;
-  BitFury: number;
-  F2Pool: number;
-  ViaBTC: number;
-  CKPool: number;
-  Unknown: number;
-  date: number;
-  username: string;
-  lat: number;
-  long: number;
-  geolocationPosition: Object;
+    // varibles
+    title: string;
+    text: string;
+    AntPool: number;
+    GBMiners: number;
+    SlushPool: number;
+    KanoPool: number;
+    BitFury: number;
+    F2Pool: number;
+    ViaBTC: number;
+    CKPool: number;
+    Unknown: number;
+    date: number;
+    username: string;
+    lat: number;
+    long: number;
+    geolocationPosition: Object;
 
   constructor(private blockchainService: BlockchainService, private statusService: StatusService) {}
 
+  // On component initialization
   ngOnInit() {
-    console.log("prices init");
+    // get pool data from service
     this.blockchainService.getPools()
        .subscribe(
         res => {
@@ -52,18 +56,21 @@ export class PoolComponent implements OnInit {
            },
            error => console.error("error:" + error)
     );
+    // get user location
     this.getLocation();
     // asign username from local storage
     this.username = this.user.username;
     console.log(this.username);
 }
 
+// set lat and long
 setPosition(position) {
   this.lat = position.coords.latitude;
   this.long = position.coords.longitude;
   console.log("Your Lat:" + this.lat + "\nYour Long" + this.long);
 }
 
+// get location of user
 getLocation() {
   if (window.navigator && window.navigator.geolocation) {
     window.navigator.geolocation.getCurrentPosition(
@@ -89,18 +96,23 @@ getLocation() {
 };
 }
 
-  onStatusPoolSubmit(){
+// POST new miner status
+onStatusPoolSubmit(){
+    // set current date
     this.date = Date.now();
+    // set new status post with miner data
     const newStatusPost = new PostPools(this.username,this.date,this.title,this.text,this.Unknown,this.GBMiners,this.SlushPool
     ,this.KanoPool,this.BitFury,this.AntPool,this.F2Pool,this.ViaBTC,this.lat,this.long);
-    console.log(newStatusPost);
+
+    // send modal to service
     this.statusService.savePoolPost(newStatusPost)
     .subscribe(
-        () => console.log('POST from status'),
+        () => console.log('POST from miner status'),
         error => console.error(error)
     );
   }
 
+  // GET username from local storage
   get user(): any {
     return JSON.parse(localStorage.getItem('user'));
   }
